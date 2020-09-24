da87846a76d856719f9d42df11b6fd3095c432ed
Rafael Roque <cinthiabastos@MacBook-Air-de-Cinthia-2.local>
2020-09-24 13:51:36 -0300

    Initial setup

---
+++ src/main/java/com/teste/spring/Java8SpringbootRestJdbiApplication.java
@@ -0,0 +1,13 @@
+package com.teste.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8SpringbootRestJdbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8SpringbootRestJdbiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/teste/spring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.teste.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testespring")
+public class HelloController {
+
+	
+	@GetMapping
+	public String index() {
+		return "teste";
+	}
+	
+}
