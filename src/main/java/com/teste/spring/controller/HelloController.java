bf3dca16631c31cef10addb4a2f4cfcd8e9d4c5c
Rafael Roque <cinthiabastos@MacBook-Air-de-Cinthia-2.local>
2020-09-24 14:07:34 -0300

    Log4j config

--- src/main/java/com/teste/spring/controller/HelloController.java
+++ src/main/java/com/teste/spring/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.teste.spring.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/testespring")
 public class HelloController {
 
-	
+	private Logger LOGGER = LogManager.getLogger(HelloController.class);
 	@GetMapping
 	public String index() {
+		LOGGER.info("chegou");
 		return "teste";
 	}
 	
